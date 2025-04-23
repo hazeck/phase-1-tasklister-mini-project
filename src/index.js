943d496154e8b2e500a1bbf736c9f4be388dbf2c
Chadrick <chadrickalvarez@gmail.com>
2025-04-23 12:22:18 -0400

    Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskDescription = event.target["new-task-description"].value;
+
+    buildToDo(taskDescription);
+  });
+
+  function buildToDo(task) {
+    const taskList = document.querySelector("#tasks");
+    const taskItem = document.createElement("li");
+    taskItem.textContent = task;
+    taskList.append(taskItem);
+  }
 });
